c56ca3530b40442b4e32e8ff8eeb09d76b0ca499
Peter Pouliot <peter@pouliot.net>
2020-04-21 11:00:43 -0400

    Updating to multi arch

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,29 @@
 class fetchfact {
 
-  file { '/etc/puppetlabs/facter':
+  case $::kernel {
+    'Linux':{
+      $local_facter_base_path = "/etc/factor"
+    }
+    'windows':{
+      $local_facter_base_path = "C:/ProgramData/PuppetLabs/facter"
+    }
+    default:{
+      $local_facter_base_path = "/etc/puppetlabs/facter"
+    }
+  }
+  
+  file { $local_factor_base_path:
     ensure => directory,
     owner  => root,
     group  => root,
     mode   => '0755'
   }
 
-  file { '/etc/puppetlabs/facter/facts.d':
+  file { "${local_factor_base_path}/facts.d":
     ensure  => directory,
     owner   => root,
     group   => root,
     mode    => '0755',
-    require => File['/etc/puppetlabs/facter']
+    require => File[$local_factor_base_path]
   }
 }
